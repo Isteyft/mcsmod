9812c74e646c05d7c4c20bb0501da200dcce2ad3
Isteyft <14056025+isteyft@user.noreply.gitee.com>
2025-10-23 23:37:58 +0800

    白泽

---
+++ IsTools/DialogEnv/Player/IS_HasKeZhanTime.cs
@@ -0,0 +1,24 @@
+﻿using KBEngine;
+using SkySwordKill.Next.DialogSystem;
+using SkySwordKill.NextMoreCommand.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using top.Isteyft.MCS.IsTools.Util;
+
+namespace top.Isteyft.MCS.IsTools.DialogEnv.Player
+{
+    [DialogEnvQuery("IS_HasKeZhanTime")]
+    public class IS_HasKeZhanTime : IDialogEnvQuery
+    {
+        public object Execute(DialogEnvQueryContext context)
+        {
+            string scenes = context.GetMyArgs(0, "");
+            KBEngine.Avatar player = Tools.instance.getPlayer();
+            bool hasTime = player.zulinContorl.HasTime(scenes);
+            return hasTime;
+        }
+    }
+}
